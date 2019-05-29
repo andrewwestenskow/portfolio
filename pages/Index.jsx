8db581c9cddfb7a0a60a7de9254c0a6a6a698a58
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-05-29 11:37:36 -0600

    Initial commit

---
+++ pages/Index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/Index.scss'
+import Link from 'next/link'
+
+const Index = () => {
+
+  return <div className='Index'>Portfolio</div>
+}
+
+export default Index
